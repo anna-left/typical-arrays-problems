62d7f325a865f47c95e102fa9f5036dcef57216b
anna-left <87468994+anna-left@users.noreply.github.com>
2021-08-09 17:05:45 +0300

    Update index.js
    feat: added functions max, min, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
+exports.min = function min(array) {
+    if ((array === undefined) || (array.length === 0)) {
+        return 0;
+    }
+    array.sort((a, b) => { return a - b });
+    return array[0];
 
-exports.min = function min (array) {
-  return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if ((array === undefined) || (array.length === 0)) {
+        return 0;
+    }
+    array.sort((a, b) => { return b - a });
+    return array[0];
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array === undefined) {
+        return 0;
+    }
+    const lArray = array.length;
+    if (lArray === 0) {
+        return 0;
+    }
+
+    let sum = 0;
+    let i = 0;
+    while (i < lArray) {
+        sum = sum + array[i];
+        i = i + 1;
+    }
+    return sum / lArray;
+
 }
